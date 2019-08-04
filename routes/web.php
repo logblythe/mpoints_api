3c2c203a246f485574ef2a69bbbc895a5cc714b0
sikshya <sikshyamaharjan123@gmail.com>
2019-08-05 01:23:26 +0545

    Procfile for Heroku

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes(['verify' => true]);
+//Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
+//Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
